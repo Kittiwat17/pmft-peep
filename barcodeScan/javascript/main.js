5ccff98c5eaefdf8f313a57ee1dc0e07f6419ae3
kittiwat <memoryeamkijkarn@gmail.com>
2020-08-15 01:28:51 +0700

    แก้ canvas

--- barcodeScan/javascript/main.js
+++ barcodeScan/javascript/main.js
@@ -30,6 +30,26 @@ function load_quagga() {
         var last_result = [];
         if(Quagga.initialized == undefined){
             console.log('defind');
+            Quagga.onProcessed(function (result) {
+                var drawingCtx = Quagga.canvas.ctx.overlay,
+                drawingCanvas = Quagga.canvas.dom.overlay;
+
+                if (result) {
+                    if (result.boxes) {
+                        drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+                        
+                    }
+
+                    if (result.box) {
+                        Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#000", lineWidth: 4 });
+                    }
+
+                    if (result.codeResult && result.codeResult.code) {
+                        Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 5 });
+                    }
+                }
+            });
+
             Quagga.onDetected(function(result){
                 console.log('detected');
                 var last_code = result.codeResult.code;
@@ -66,10 +86,10 @@ function load_quagga() {
                 target: document.querySelector('#barcode-scanner')  // Or '#yourElement' (optional)
             },
             decoder: {
-                readers: ["code_39_reader", "code_39_vin_reader"]
+                // readers: ["code_39_reader", "code_39_vin_reader"]
 
                 //readers: ["code_128_reader" ]
-                //readers: ["ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "code_128_reader"]
+                readers: ["ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "code_128_reader"]
             }
         }, function (err) {
             if (err) {
