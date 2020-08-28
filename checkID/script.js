30284130cd7697b715b563b14c574684df30e994
kittiwat <memoryeamkijkarn@gmail.com>
2020-08-28 22:14:20 +0700

    checkID

--- checkID/script.js
+++ checkID/script.js
@@ -1,21 +1,27 @@
 var acceptBtn = document.querySelector('#accept');
 var fnameBar, lnameBar, emailBar
-var fname, lname, email = '';
+var fname, lname, email, codeID = '';
+var isFindUser = false;
 acceptBtn.addEventListener('click', () => {
-    if (false) {//ถ้าหาเจอทำif หาในระบบไม่เจอใช้else
-        // query email & barcode on firebase
-        fname = document.querySelector("#firstName").value;
-        console.log(fname);
-        lname = document.querySelector("#lastName").value;
-        console.log(lname)
+    //ถ้าหาเจอทำif หาในระบบไม่เจอใช้else
+    // query email & barcode on firebase
+    fname = document.querySelector("#firstName").value;
+    lname = document.querySelector("#lastName").value;
+
+    // codeID = ไอดีของผู้ใช้
+    if (isFindUser) {
         fnameBar = document.querySelector("#fname");
         lnameBar = document.querySelector("#lname");
-
         emailBar = document.querySelector("#email");
         fnameBar.innerHTML = fname;
         lnameBar.innerHTML = lname;
         // emailBar.innerHTML = email;
-    }else{
+
+
+        JsBarcode("#showBarcode", codeID);
+        var imgBarCode = document.querySelector('#showBarcode')
+        var btnBarCode = document.querySelector('.saveImg').href = imgBarCode.src;
+    } else {
         $("#failedModal").modal('show');
     }
 })
